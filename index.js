00809bde714d46044cc2e0ae1379ec325e80d091
Sara Grimes <sarajoy415@gmail.com>
2019-07-01 01:00:19 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string,spy){
+  console.log(string.toUpperCase())}
+  
+function logWhisper(string,spy){
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string){
+var lowercase = "hello".toLowerCase()
+var uppercase = "HELLO".toUpperCase()
+var love = "I love you, Grandma." === "I love you, too"
+if(string === lowercase){
+  return "I can't hear you!"}
+else if(string === uppercase){
+  return "YES INDEED!"}
+else(string === love)
+  return "I love you, too"
+}
+
+
+
